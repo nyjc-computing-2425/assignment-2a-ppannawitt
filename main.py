43b109c27424f79aa09d49120a632f26b2319122
ppannawitt <106550775+ppannawitt@users.noreply.github.com>
2024-03-06 06:23:34 +0000

    submit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 num = input('Enter a number (decimal or integer): ')
 # type your code here
-
+num = num.strip()
+num1 = num.replace(".", "")
+num1 = num1.lstrip("0")
+sf = len(num1)
 
 
 # do not change any code beyond this point
